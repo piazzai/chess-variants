10757607974191a041624bd4d6da0c02258ff452
piazzai <piazzai@pm.me>
2022-02-22 17:46:00 +0100

    Check interaction effect

--- scripts/additional.R
+++ scripts/additional.R
@@ -2,6 +2,8 @@ source("global.R")
 
 load("logit.RData")
 
+# predict probabilities of winning
+
 sim <- data.table(
   rating = 1500,
   gap = c(0, 25),
@@ -25,6 +27,8 @@ sim$se <-
   predict(logit$fit, sim, se.fit = T, type = "response")$se.fit
 sim$ci1 <- with(sim, fit - 1.96 * se)
 sim$ci2 <- with(sim, fit + 1.96 * se)
+
+# compare empirical and expected scores
 
 load("pgn.RData")
 
@@ -93,6 +97,68 @@ gap <- ggplot(score) +
 ggsave(
   gap,
   filename = "expectedscore.jpg",
+  height = 1024,
+  width = 1280,
+  units = "px",
+  dpi = 150
+)
+
+# check interactions with player rating
+
+logit$fit2 <-
+  glm(
+    win ~ rating + gap + white + total + faster + slower + antichess + atomic * rating +
+      chess960 + crazyhouse + horde + kingofthehill + racingkings + threecheck,
+    data = logit$fit$data,
+    family = binomial(link = "logit")
+  )
+
+sim <- data.table(
+  rating = quantile(logit$fit$data$rating, c(.04, .5, .96)) %>%
+    round(-2) %>%
+    rep(each = 11),
+  gap = 0,
+  white = 1,
+  total = 20,
+  faster = 0,
+  slower = 0,
+  antichess = 0,
+  atomic = seq(0, 1, .1) %>% rep(3),
+  chess960 = 0,
+  crazyhouse = 0,
+  horde = 0,
+  kingofthehill = 0,
+  racingkings = 0,
+  threecheck = 0
+)
+
+sim$fit <-
+  predict(logit$fit2, sim, se.fit = T, type = "response")$fit
+sim$se <-
+  predict(logit$fit2, sim, se.fit = T, type = "response")$se.fit
+sim$ci1 <- with(sim, fit - 1.96 * se)
+sim$ci2 <- with(sim, fit + 1.96 * se)
+
+atomic <- ggplot(sim) +
+  geom_line(aes(atomic, fit, group = rating, lty = factor(rating))) +
+  geom_ribbon(aes(
+    atomic,
+    ymin = ci1,
+    ymax = ci2,
+    group = rating
+  ), alpha = .2) +
+  scale_x_continuous(breaks = seq(0, 1, .2), labels = paste0(100 * seq(0, 1, .2), "%")) +
+  scale_y_continuous(
+    limits = c(.2, .6),
+    breaks = seq(.2, .6, .05),
+    labels = paste0(100 * seq(.2, .6, .05), "%")
+  ) +
+  labs(x = "Share of games played in Atomic", y = "Probability of winning", lty = "Player rating") +
+  custom_theme
+
+ggsave(
+  atomic,
+  filename = "atomicrating.jpg",
   height = 1024,
   width = 1280,
   units = "px",
